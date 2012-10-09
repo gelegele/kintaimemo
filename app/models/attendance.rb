10803066744c7255a37e5e4903165b34e87bf099
gelegele <ryo500209@gmail.com>
2012-10-09 16:06:47 +0900

    Added validation to attendance.

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -5,4 +5,11 @@ class Attendance
   field :in, :type => Time
   field :out, :type => Time
   field :note, :type => String
+
+  validates_presence_of :date
+  validates_uniqueness_of :date, message: 'already exists.'
+  validates_each :in do |model, attr, value|
+    model.errors.add(attr, 'must be less than out.') if model.in && model.out && model.out <= model.in
+  end
+
 end
